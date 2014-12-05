5335ca839d5f42d1020c2abd424ac1c7b10d8d70
Adam Cooke <me@adamcooke.io>
2014-12-05 14:09:28 +0000

    Add Luxembourg's new 2015 taxs

--- rates/luxembourg.rb
+++ rates/luxembourg.rb
@@ -4,6 +4,15 @@ country do
   code 'LU'
   
   period do
+    effective_from 2015, 1, 1
+    rate :super_reduced, 3
+    rate :reduced1, 8
+    rate :reduced2, 14
+    rate :standard, 17
+    rate :parking, 12
+  end
+  
+  period do
     rate :super_reduced, 3
     rate :reduced1, 6
     rate :reduced2, 12
