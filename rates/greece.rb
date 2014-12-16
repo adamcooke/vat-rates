45d338adf002cb0e9852e9e84cd3a6b6bd9a3be9
Adam Cooke <me@adamcooke.io>
2014-12-16 13:50:17 +0000

    please stop requesting changes to Greece's code

--- rates/greece.rb
+++ rates/greece.rb
@@ -1,6 +1,10 @@
 country do
   
   name 'Greece'
+  
+  # This is correct. Please do not submit pull requests to change this code. 
+  # The EC have decied this is an appropriate code. Full details at:
+  # http://ec.europa.eu/taxation_customs/resources/documents/taxation/vat/how_vat_works/rates/vat_rates_en.pdf
   code 'EL'
   
   period do
