111fc9d4e370870b34e7b8ea45b98efe09b17476
Adam Cooke <me@adamcooke.io>
2015-07-08 15:15:31 +0100

    update polish vat rate from 1/1/16
    http://www.vatlive.com/european-news/poland-cuts-vat-from-23-to-22-january-2016/?utm_source=VAT+News+Oct-Nov&utm_campaign=438c5d38a1-Polish_VAT_cut&utm_medium=email&utm_term=0_fd72a6cb5d-438c5d38a1-126007797

--- rates/poland.rb
+++ rates/poland.rb
@@ -5,9 +5,17 @@ country do
   country_code 'PL'
 
   period do
+    effective_from 2016, 01, 01
+    rate :reduced1, 5
+    rate :reduced2, 8
+    rate :standard, 22
+  end
+
+  period do
     rate :reduced1, 5
     rate :reduced2, 8
     rate :standard, 23
   end
+  
 
 end
