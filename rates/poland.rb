8f48a022b6106f87b86e5873ea90b89bc70ceef8
Adam Cooke <me@adamcooke.io>
2015-11-13 13:05:32 +0000

    revert poland rate change

--- rates/poland.rb
+++ rates/poland.rb
@@ -5,13 +5,6 @@ country do
   country_code 'PL'
 
   period do
-    effective_from 2016, 01, 01
-    rate :reduced1, 5
-    rate :reduced2, 8
-    rate :standard, 22
-  end
-
-  period do
     rate :reduced1, 5
     rate :reduced2, 8
     rate :standard, 23
