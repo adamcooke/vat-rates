25ccdeee9ed0365d96f0e135294ec9a7dc65e349
Christian Szardenings <chris@szar.de>
2016-01-06 18:11:52 +0100

    Revert VAT change for Estonia (ES)
    There is currently no reliable source on the estonian VAT change. Hence
    I reverted that change.

--- rates/estonia.rb
+++ rates/estonia.rb
@@ -5,12 +5,6 @@ country do
   country_code "EE"
 
   period do
-    effective_from 2016, 1, 1
-    rate :reduced, 14
-    rate :standard, 20
-  end
-
-  period do
     rate :reduced, 9
     rate :standard, 20
   end
