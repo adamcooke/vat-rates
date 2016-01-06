9214f39255a8ae4af240a57011d2fdafd1d39905
Christian Szardenings <chris@szar.de>
2016-01-06 02:02:52 +0100

    Reflect VAT changes in Estonia & Austria
    Since Jan 1, 2016, the following changes are in effect:
    
    Austria
    Reducted rate increased from 10% to 13%
    
    Estonia
    Reduced rate increased from 9% to 14%

--- rates/austria.rb
+++ rates/austria.rb
@@ -5,6 +5,13 @@ country do
   country_code 'AT'
 
   period do
+    effective_from 2016, 1, 1
+    rate :reduced, 13
+    rate :standard, 20
+    rate :parking, 12
+  end
+
+  period do
     rate :reduced, 10
     rate :standard, 20
     rate :parking, 12
--- rates/estonia.rb
+++ rates/estonia.rb
@@ -5,6 +5,12 @@ country do
   country_code "EE"
 
   period do
+    effective_from 2016, 1, 1
+    rate :reduced, 14
+    rate :standard, 20
+  end
+
+  period do
     rate :reduced, 9
     rate :standard, 20
   end
