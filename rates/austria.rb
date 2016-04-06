2068af021a7f8b020f67fcff4235d2282a499257
Adam Cooke <me@adamcooke.io>
2016-04-06 09:42:19 +0100

    update austria's 2016 rates

--- rates/austria.rb
+++ rates/austria.rb
@@ -5,6 +5,14 @@ country do
   country_code 'AT'
 
   period do
+    effective_from 2016, 1, 1
+    rate :reduced1, 10
+    rate :reduced2, 13
+    rate :standard, 20
+    rate :parking, 13
+  end
+
+  period do
     rate :reduced, 10
     rate :standard, 20
     rate :parking, 12
