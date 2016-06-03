55f8bb09218d72f248cddc2d4a1ce3ac81041b4e
pl-jankowskimichal <pl.jankowskimichal@gmail.com>
2016-06-03 11:13:30 +0200

    Update greece.rb

--- rates/greece.rb
+++ rates/greece.rb
@@ -7,7 +7,14 @@ country do
   # The EC have decied this is an appropriate code. Full details at:
   # http://ec.europa.eu/taxation_customs/resources/documents/taxation/vat/how_vat_works/rates/vat_rates_en.pdf
   code 'EL'
-
+  
+  period do
+    effective_from 2016, 6, 1
+    rate :reduced1, 6
+    rate :reduced2, 13.5
+    rate :standard, 24
+  end
+  
   period do
     effective_from 2016, 1, 1
     rate :reduced1, 6
