3f0e84f2a2b0a215ffa3d742e4ee86e593bee2fc
Beno!t POLASZEK <bpolaszek@users.noreply.github.com>
2017-02-23 12:18:26 +0100

    Update france.rb
    Source (fr): https://fr.wikipedia.org/wiki/Taxe_sur_la_valeur_ajout%C3%A9e_en_France

--- rates/france.rb
+++ rates/france.rb
@@ -5,10 +5,25 @@ country do
   country_code 'FR'
 
   period do
+    effective_from 2014, 1, 1
     rate :super_reduced, 2.1
     rate :reduced1, 5.5
     rate :reduced2, 10
     rate :standard, 20
+  end  
+
+  period do
+    effective_from 2012, 1, 1
+    rate :super_reduced, 2.1
+    rate :reduced1, 5.5
+    rate :reduced2, 7
+    rate :standard, 19.6
+  end
+
+  period do
+    rate :super_reduced, 2.1
+    rate :reduced1, 5.5
+    rate :standard, 19.6
   end
 
 end
