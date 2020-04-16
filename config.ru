2860a93fd94d107325f26814a4a98564c69b0d99
Adam Cooke <me@adamcooke.io>
2020-04-16 19:35:43 +0000

    Goodbye again - might as well remove all the files

--- config.ru
+++
@@ -1,4 +0,0 @@
-$:.unshift File.expand_path(File.join('..', 'lib'), __FILE__)
-require 'vat_rates'
-require 'vat_rates/server'
-run VATRates::Server.new
--- puma.rb
+++
@@ -1,6 +0,0 @@
-threads 5, 5
-port        ENV.fetch("PORT") { 3000 }
-environment ENV.fetch("RACK_ENV") { "development" }
-quiet false
-prune_bundler
-directory ENV['APP_ROOT'] if ENV['APP_ROOT']
